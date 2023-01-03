24e0fc937f569a07b838192905be50c341ab8884
KaemsaiPrarisa <prarisa_s@cmu.ac.th>
2023-01-03 13:46:11 +0700

    lab07

--- app/views.py
+++ app/views.py
@@ -110,8 +110,13 @@ def lab06_courses():
     return render_template('lab06/courses.html', course_list=course_list)
 
 
+@app.route('/lab07')
+def lab07_form_validation():
+    return app.send_static_file('lab07_form_validation.html')
 
-
+@app.route('/lab07b')
+def lab07b():
+    return app.send_static_file('lab07b.html')
 
 
 
